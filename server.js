d99993bd66b30eeb148dcaf99751297dc0afdd1e
Tom Norman <tom88norman@hotmail.co.uk>
2024-10-27 13:39:28 +0000

    added domain to session config in server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: { 
         secure: config.IS_PRODUCTION,
+        domain: '.subsmash.io',
         maxAge: 1000 * 60 * 60 * 24 * 7
      }
 }));
